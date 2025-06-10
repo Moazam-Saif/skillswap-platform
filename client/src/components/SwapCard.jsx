fe5e17ad544ce24df6df5b6f8c5669068de3be59
Moazam-Saif <msaif.bese23seecs@seecs.edu.pk>
2025-06-10 16:15:38 +0500

    ...

--- client/src/components/SwapCard.jsx
+++ client/src/components/SwapCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { ReactComponent as RightEllipse } from '../assets/RightEllipse.svg';
 
 
 export const SwapCard = () => {
@@ -9,51 +10,44 @@ export const SwapCard = () => {
                 {/* SVG placeholders */}
                 <div className="absolute left-[-2px] top-0 h-[122px] w-[126px] flex items-center justify-center"
                     style={{
-                        backgroundImage: "url('/left ellipse.svg')",
+                        backgroundImage: "url('/LeftEllipse.svg')",
                         backgroundRepeat: "no-repeat",
-                        backgroundSize: "contain",
-                        backgroundPositionX:"left"
+                        backgroundSize: "contain"
+
+                    }}>
+                        <text className='text-2xl z-50 rotate-90'>Hello</text>
+                </div> 
+                <div className='absolute left-0 bottom-0 h-[8px] w-[8px] bg-[#F4A261]'></div>
+                 <div
+                    className="absolute right-[-2px] top-0 h-[122px] w-[126px] flex items-center justify-center"
+                    style={{
+                        backgroundImage: "url('/RightEllipse.svg')",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "contain"
                     }}
                 >
-                    
-                    {/* Replace with your left SVG */}
-                    {/* <YourLeftSVG /> */}
-                </div>
-                <div className='absolute left-0 bottom-0 h-[8px] w-[8px] bg-[#F4A261]'></div>
-                <div className="absolute right-[-2px] top-0 h-[122px] w-[126px] flex items-center justify-center"
-                    style={{
-                        backgroundImage: "url('/right ellipse.svg')",
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "contain",
-                        backgroundPositionX: "right"
-                    }}>
-                    
-
-                    {/* Replace with your right SVG */}
-                    {/* <YourRightSVG /> */}
 
                 </div>
-                <div className='absolute right-0 bottom-0 h-[8px] w-[8px] bg-[#E9C46A]'></div>
-                <div className="text-white text-4xl">↔</div>
-            </div>
 
-            {/* Bottom Section */}
-            <div className="w-full h-[50px] flex bg-[#E76F51] rounded-b-[30px] overflow-hidden">
-                {/* Left: Image */}
-                <div className="w-1/2 flex items-center justify-center border-r-2 border-white">
-                    <img
-                        src="https://via.placeholder.com/80"
-                        alt="Profile"
-                        className="w-16 h-16 rounded-full object-cover"
-                    />
-                </div>
-                {/* Right: Name */}
-                <div className="w-1/2 flex flex-col items-center justify-center text-[#2E2E2E] font-semibold text-lg px-2 text-center">
-                    <p>Moazam</p>
-                    <p>Saif</p>
-                </div>
-            </div>
+ 
+            <div className='absolute right-0 bottom-0 h-[8px] w-[8px] bg-[#E9C46A]'></div>
         </div>
+    <div className="w-full h-[50px] flex bg-[#E76F51] rounded-b-[30px] overflow-hidden">
+        {/* Left: Image */}
+        <div className="w-1/2 flex items-center justify-center border-r-2 border-white">
+            <img
+                src="https://via.placeholder.com/80"
+                alt="Profile"
+                className="w-16 h-16 rounded-full object-cover"
+            />
+        </div>
+        {/* Right: Name */}
+        <div className="w-1/2 flex flex-col items-center justify-center text-[#2E2E2E] font-semibold text-lg px-2 text-center">
+            <p>Moazam</p>
+            <p>Saif</p>
+        </div>
+    </div>
+        </div >
     );
 };
 
